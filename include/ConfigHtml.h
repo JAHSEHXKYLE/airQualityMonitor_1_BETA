23ccd3aa4b29b04a494a5a0b2056d96f854d320a
JahsehKYN <little_li030315@foxmail.com>
2025-01-18 21:45:00 +0800

    initConfig页面测试成功

--- include/ConfigHtml.h
+++ include/ConfigHtml.h
@@ -3,7 +3,424 @@
 #define CONFIGHTML_H
 
 namespace ConfigHtml {
-    String configHtml = R"rawliteral(
+    
+    String initHtml = R"rawliteral(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wifi Scan</title>
+</head>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        color: rgb(82, 82, 82);
+        font-family: 'Microsoft YaHei', Arial, sans-serif;
+        font-size: 3rem;
+    }
+
+    body {
+        position: fixed;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        width: 100vw;
+        opacity: 0;
+        transition: opacity 5s ease;
+    }    
+    .main {
+        display: flex;
+        flex-direction: column;    /* 垂直排列 */
+        justify-content: center;  /* 居中 */
+        align-items: center;     /* 居中 */
+        height: 100rem;
+        width: 80rem;
+    }
+
+    .top {
+        user-select: none;
+        font-size: 4rem;
+        position: relative;  
+        width: 100%;
+        height: 15rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+        background-color: #eeeeee;
+        border-radius: 20px;
+        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+    }
+    .inner {  /* wifi列表 */
+        margin-top: 1rem;  /* 距离顶部的距离 */
+        padding: 2.5rem 0rem;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-radius: 20px;
+        background-color: #eeeeee;
+        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+        z-index: 1;
+        overflow-y: auto;
+        scrollbar-width: none;
+        outline: 2rem solid #eeeeee;   
+        outline-offset: -2rem;
+    }
+
+    .list_item {  /* wifi列表项 */
+        user-select: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 2rem 5rem;
+        height: 15rem;
+        font-size: 3rem;
+        font-weight: bold;
+        border-radius: 2rem;
+        background-color: rgb(255, 255, 255);
+    }
+    .list-item:hover {
+        background-color: #727272;
+    }
+    .list_item_error {  /*未找到网络连接*/
+        user-select: none;
+        text-align: center;
+        margin: 50% 5px;
+    }
+
+
+    .tail {
+        user-select: none;
+        bottom: 0;  
+        width: 100%;
+        height: 10rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 2rem;
+        color: rgb(82, 82, 82);
+    }
+    .bg {
+        pointer-events: none;  /* 禁止点击背景 */
+        user-select: none;    /* 禁止选中文本 */
+        height: 10%;
+        width: 200%;
+        display: flex;
+        flex-direction: column;
+        position: absolute;  
+        justify-items: center;  
+        align-items: center;  
+        z-index: 1001;
+        transform: rotate(0deg);
+    }
+
+    .bg-text {
+        user-select: none;
+        font-weight: 700;
+        font-size: 15rem;
+        color: rgba(116, 116, 116, 0.5);
+    }
+
+    #modal-overlay {
+        user-select: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(141, 141, 141, 0.5);
+        z-index: 999;
+        display: none;
+        backdrop-filter: blur(10px);
+    }
+    #modal {
+        user-select: none;
+        display: none;
+        position: fixed;
+        flex-direction: column;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 60rem;
+        padding: 3rem 5rem 1rem 5rem;
+        background-color: white;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+        border-radius: 8px;
+        z-index: 1000;
+    }
+    #item-content {
+        user-select: none;
+        font-size: 3rem;
+        font-weight: bold;
+        margin-bottom: 1.5rem;
+    }
+    .item-inners {
+        display: flex;  
+        justify-content: center;
+        align-items: center;
+    }
+    #modal input {
+        box-sizing: border-box;
+        height: 6rem;
+        width: 100%;
+        padding: 1rem;
+        border-radius: 5px;
+        border: none;
+        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+    }
+    #modal button {
+        user-select: none;
+        vertical-align: top;  /* 垂直居中 */
+        height: 6rem;
+        width: 20%;
+        margin-left: 5%;
+        border-radius: 5px;
+        border: none;
+        background-color:black;
+        color: white;
+        cursor: pointer;
+        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+    }
+    #modal button:hover {
+        background-color: white;
+        color: black;
+    }
+    #item-error-message {
+        user-select: none;
+        color: red;
+        font-size: 2rem;
+        margin-top: 1.5rem;
+        opacity: 0;
+        transition: opacity 0.5s ease;
+    }
+
+
+
+    
+</style>
+<body>
+    <div class="bg"><p class="bg-text">JAHSEHKYLE KUIBOYANG</p></div> <!-- 水印文字 -->
+    <div id="modal-overlay"></div>
+    <div id="modal">
+        <div id="item-content">SSID: xxxxxx</div>
+        <div class ="item-inners">
+            <input type="password" id="password-input" placeholder="请输入密码">
+            <button id="submit-button">取消</button>
+        </div>
+        <div id="item-error-message">"响应超时，请重新输入！"</div>
+    </div>
+    <div class="main">
+        <div class="top">WLAN</div>
+        <div class="inner"></div>
+        </div> <!-- wifi列表 -->
+    </div>
+    <div class="tail">
+        ip:192.168.0.1 From JAHSEHKYLE KUIBOYANG
+    </div>
+</body>
+<script>
+    async function get_wifi_data() {
+        try {
+            const response = await fetch('/get_wifi_data');
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            console.log("获取WiFi数据成功");
+            var data = await response.json();
+            set_wifi_list(data);
+            console.log(data);
+        } catch (error) {
+            console.error('获取WiFi数据时出错:', error);
+            var data = {};
+            set_wifi_list(data);
+        }
+    }
+
+
+    const wifi_list = document.querySelector('.inner');
+    const item_input = document.getElementById('password-input');
+    const submit_button = document.getElementById('submit-button');
+    const modal = document.getElementById('modal');
+
+    item_input.addEventListener('input',() => {  // 密码输入框事件
+        if (item_input.value.trim() !== ''){
+            submit_button.textContent = '确定';
+            submit_button.style.backgroundColor = 'white';
+            submit_button.style.color = 'black';
+        } else {
+            submit_button.textContent = '取消';
+            submit_button.style.backgroundColor = 'black';
+            submit_button.style.color = 'white';
+        }
+    });
+
+    submit_button.addEventListener('click',() => {  // 确定按钮事件
+        const botton_text = submit_button.textContent;
+        const password = item_input.value.trim();
+        const link_wifi_name = document.getElementById('item-content').textContent.split(':')[1].trim();
+        if (botton_text === '确定'){
+            alert("连接的wifi名称：" + link_wifi_name + "\n密码：" + password);
+        } else {
+            hide_modal();
+        }
+    });
+
+    function set_wifi_list(wifi_json_data) {
+        if (Object.keys(wifi_json_data).length == 0){  // 无可用网络
+            const list_item_error = document.createElement('div');
+            list_item_error.className = 'list_item_error';
+            list_item_error.textContent = '未找到任何可用网络，请刷新后重试！';
+            wifi_list.appendChild(list_item_error);
+        } else {
+            for (let i = 0; i < wifi_json_data.wifi_names.length; i++) {  // 循环创建wifi列表项
+                const list_item = document.createElement('div');
+                list_item.style.display = 'flex';
+                list_item.style.flexDirection = 'column';
+                list_item.className = 'list_item';
+                list_item.addEventListener('click',() => {
+                    show_modal(wifi_json_data.wifi_names[i]);
+                });
+                const list_item_top = document.createElement('div');  // wifi名称和强度
+                list_item_top.style.display = 'flex';
+                list_item_top.style.justifyContent = 'center';
+                list_item_top.style.flexDirection = 'row';
+                const list_item_top_wifi_name = document.createElement('p');  // wifi名称
+                list_item_top_wifi_name.textContent = `${wifi_json_data.wifi_names[i]}`;
+                list_item_top_wifi_name.style.fontSize = '4.5rem';
+                list_item_top_wifi_name.style.width = '50rem';
+                list_item_top_wifi_name.style.height = '5rem';
+                list_item_top_wifi_name.style.marginRight = '1rem';
+                // list_item_top_wifi_name.style.backgroundColor = 'white';
+                list_item_top_wifi_name.style.display = 'flex';
+                list_item_top_wifi_name.style.alignItems = 'center';
+                const list_item_top_wifi_strength = document.createElement('p');  // wifi强度
+                list_item_top_wifi_strength.textContent = `${wifi_json_data.wifi_rssi[i]} dBm`;
+                list_item_top_wifi_strength.style.fontSize = '2rem';
+                list_item_top_wifi_strength.style.width = '9rem';
+                //list_item_top_wifi_strength.style.backgroundColor = 'white';
+                list_item_top_wifi_strength.style.display = 'flex';
+                list_item_top_wifi_strength.style.alignItems = 'center';
+                list_item_top.appendChild(list_item_top_wifi_name);
+                list_item_top.appendChild(list_item_top_wifi_strength);
+                list_item.appendChild(list_item_top);
+                const list_item_data = document.createElement('div');  // wifi数据
+                list_item_data.style.display = 'flex';
+                list_item_data.style.justifyContent = 'center';
+                list_item_data.style.marginTop = '1rem';
+                list_item.appendChild(list_item_data);
+                const list_item_data_encryptedTYPE = document.createElement('p');  // 加密类型
+                list_item_data_encryptedTYPE.style.fontSize = '1.5rem';
+                list_item_data_encryptedTYPE.textContent = `加密类型: ${wifi_json_data.wifi_encryptedTYPE[i]}`;
+                list_item_data_encryptedTYPE.style.width = '24rem';
+                list_item_data_encryptedTYPE.style.height = '5rem';
+                list_item_data_encryptedTYPE.style.marginRight = '1rem';
+                // list_item_data_encryptedTYPE.style.backgroundColor = 'white';
+                list_item_data_encryptedTYPE.style.display = 'flex';
+                list_item_data_encryptedTYPE.style.alignItems = 'center';
+                list_item_data.appendChild(list_item_data_encryptedTYPE);
+                const list_item_data_channel = document.createElement('p');  // 信道
+                list_item_data_channel.style.fontSize = '1.5rem';
+                list_item_data_channel.textContent = `信道: ${wifi_json_data.wifi_channel[i]}`;
+                list_item_data_channel.style.width = '8rem';
+                list_item_data_channel.style.height = '5rem';
+                list_item_data_channel.style.marginRight = '1rem';
+                // list_item_data_channel.style.backgroundColor = 'white';
+                list_item_data_channel.style.display = 'flex';
+                list_item_data_channel.style.alignItems = 'center';
+                list_item_data.appendChild(list_item_data_channel);
+                const list_item_data_MAC = document.createElement('p');  // MAC地址
+                list_item_data_MAC.style.fontSize = '1.5rem';
+                list_item_data_MAC.textContent = `MAC地址: ${wifi_json_data.wifi_MAC[i]}`;
+                list_item_data_MAC.style.width = '25rem';
+                list_item_data_MAC.style.height = '5rem';
+                list_item_data_MAC.style.marginRight = '1rem';
+                // list_item_data_MAC.style.backgroundColor = 'white';
+                list_item_data_MAC.style.display = 'flex';
+                list_item_data_MAC.style.alignItems = 'center';
+                list_item_data_MAC.style.justifyContent = 'flex-end';
+                list_item_data.appendChild(list_item_data_MAC);
+                wifi_list.appendChild(list_item);
+            }
+        }
+    }
+
+    function show_modal(ssid) {
+        document.getElementById('item-content').textContent = `SSID: ${ssid}`;
+        modal.style.display = 'flex';  // 显示弹窗框
+        document.getElementById('modal-overlay').style.display = 'block';  // 显示遮罩层
+    }
+
+    function hide_modal() {
+        modal.style.display = 'none';  // 隐藏弹窗框
+        document.getElementById('modal-overlay').style.display = 'none';  // 隐藏遮罩层
+    }
+
+
+    function resetHTMLFontSize() {
+        if (screen.width <= screen.height) {
+            document.documentElement.style.fontSize = screen.width / 100 + 'px';
+        } else {
+            document.documentElement.style.fontSize = screen.height / 200 + 'px';
+        }
+        
+    }
+
+    function LogoRotation() {
+        rotationAngle = Math.atan(screen.height / screen.width) * 57.3;
+        const logo = document.querySelector('.bg');
+        logo.style.transform = `rotate(${rotationAngle}deg)`;
+    }
+
+    window.onresize = function() {
+        resetHTMLFontSize();
+        LogoRotation();
+    }
+
+    setTimeout(() => {  // 主页面淡入动画
+        document.querySelector('body').style.opacity = 1;
+    }, 100);
+
+    LogoRotation();
+    resetHTMLFontSize();
+
+    get_wifi_data();
+
+    function loop() {
+        get_wifi_data();
+    }
+
+    // setInterval(loop, 10000);  // 定时刷新
+</script>
+</html>
+)rawliteral";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+String configHtml = R"rawliteral(
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -420,5 +837,6 @@ namespace ConfigHtml {
 </html>
 )rawliteral";
 }
+
 
 #endif // CONFIGHTML_H
--- src/WifiScan.cpp
+++ src/WifiScan.cpp
@@ -2,13 +2,14 @@
 #include <WebServer.h>
 #include <EEPROM.h>
 #include <SPIFFS.h>  //存储html文件
+#include "ConfigHtml.h"
 
 #define SSID_SIZE 32
 #define PASSWORD_SIZE 64
 #define MAX_WIFI_COUNT 12 // 最大保存的 WiFi 数量 最大不超过255个
 
-const char *ssid = "ESP32_S3_BATA";
-const char *password = "12345678";
+const char *ssid = "YANG_PC";
+const char *password = "K2003@0315#";
 
 struct WifiInfo {
     String ssid;
@@ -26,15 +27,15 @@ WifiList wifiList;
 WebServer server(80);
 
 String GetWifiListjson();
+void handleRoot();
 
-void hendleRoot() {
-    File file = SPIFFS.open("/Device_Init.html", "r");
-    if (file) {
+void handleRoot() {
+    File file = SPIFFS.open("/index.html", "r");
+    if (!file) {
         server.send(404, "text/plain", "File Not Found");
-        file.close();
         return;
     }
-    server.streamFile("/Device_Init.html");
+    server.streamFile(file, "text/html");
     file.close();
 }
 
@@ -58,27 +59,28 @@ String GetWifiListjson() {
     if (count == 0) {
         json += "}"; // 没有找到 WiFi 则返回空列表
         return json;
-    }
-    for (uint8_t i = 0; i < count; i++) {
-        if (WiFi.getNetworkInfo(i, wifiList.info[i].ssid, wifiList.info[i].encType, wifiList.info[i].rssi, wifiList.info[i].bssid, wifiList.info[i].channel)) {
-            wifi_names += "\"" + wifiList.info[i].ssid + "\",";
-            wifi_encryptedTYPE += "\"" + envTYPE[wifiList.info[i].encType] + "\",";
-            wifi_channel += "\"" + String(wifiList.info[i].channel) + "\",";
-            wifi_rssi += "\"" + String(wifiList.info[i].rssi) + "\",";
-            char bssidStr[18]; // 18 字符用于存储 BSSID 的字符串表示，包括冒号和终止符
-            sprintf(bssidStr, "%02X:%02X:%02X:%02X:%02X:%02X", 
-                    wifiList.info[i].bssid[0], wifiList.info[i].bssid[1], wifiList.info[i].bssid[2], 
-                    wifiList.info[i].bssid[3], wifiList.info[i].bssid[4], wifiList.info[i].bssid[5]);
-            wifi_MAC += "\"" + String(bssidStr) + "\",";
+    } else{
+        for (uint8_t i = 0; i < count; i++) {
+            if (WiFi.getNetworkInfo(i, wifiList.info[i].ssid, wifiList.info[i].encType, wifiList.info[i].rssi, wifiList.info[i].bssid, wifiList.info[i].channel)) {
+                wifi_names += "\"" + wifiList.info[i].ssid + "\",";
+                wifi_encryptedTYPE += "\"" + envTYPE[wifiList.info[i].encType] + "\",";
+                wifi_channel += "\"" + String(wifiList.info[i].channel) + "\",";
+                wifi_rssi += "\"" + String(wifiList.info[i].rssi) + "\",";
+                char bssidStr[18]; // 18 字符用于存储 BSSID 的字符串表示，包括冒号和终止符
+                sprintf(bssidStr, "%02X:%02X:%02X:%02X:%02X:%02X", 
+                        wifiList.info[i].bssid[0], wifiList.info[i].bssid[1], wifiList.info[i].bssid[2], 
+                        wifiList.info[i].bssid[3], wifiList.info[i].bssid[4], wifiList.info[i].bssid[5]);
+                wifi_MAC += "\"" + String(bssidStr) + "\",";
+            }
         }
+        json += wifi_names.substring(0, wifi_names.length() - 1) + "],";
+        json += wifi_encryptedTYPE.substring(0, wifi_encryptedTYPE.length() - 1) + "],";
+        json += wifi_channel.substring(0, wifi_channel.length() - 1) + "],";
+        json += wifi_rssi.substring(0, wifi_rssi.length() - 1) + "],";
+        json += wifi_MAC.substring(0, wifi_MAC.length() - 1) + "]";
+        json += "}";
+        return json;
     }
-    json += wifi_names.substring(0, wifi_names.length() - 1) + "],";
-    json += wifi_encryptedTYPE.substring(0, wifi_encryptedTYPE.length() - 1) + "],";
-    json += wifi_channel.substring(0, wifi_channel.length() - 1) + "],";
-    json += wifi_rssi.substring(0, wifi_rssi.length() - 1) + "],";
-    json += wifi_MAC.substring(0, wifi_MAC.length() - 1) + "]";
-    json += "}";
-    return json;
 }
 
 void setup() {
@@ -87,14 +89,18 @@ void setup() {
         Serial.println("SPIFFS Mount Failed");
         return;
     }
-    WiFi.softAP(ssid, password);
-
-    Serial.println("WiFi Scan...\n");
-    Serial.println(GetWifiListjson());
-    Serial.println("WiFi Scan Done.");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) { // 等待连接WIFI 直到连接成功 退出循环
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println(WiFi.localIP());
+    server.on("/", []() {server.send(200, "text/html", ConfigHtml::initHtml);}); 
+    server.on("/get_wifi_data", []() {server.send(200, "application/json", GetWifiListjson());});
+    server.begin();
 }
     
 
 void loop() {
-  // 暂时没有内容
+    server.handleClient();
 }
